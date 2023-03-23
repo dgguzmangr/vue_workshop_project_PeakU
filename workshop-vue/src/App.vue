aa4a129580fbcb8fd14893ae9557466932776e52
dgguzmangr <dgguzmangr@gmail.com>
2023-03-23 10:19:19 -0500

    initial commit

---
+++ workshop-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero illo harum, architecto porro, voluptas a vel commodi quibusdam veniam in optio voluptatem totam. Sequi maxime veniam veritatis soluta eveniet in?</p>
+
+    <HelloWorld msg="Hola" :numero="5"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ workshop-vue/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} {{ numero }}</h1>
+    {{ mivariable }}
+    <br>
+    <br>
+
+    <h2>Directiva v-for</h2>
+
+    <ul>
+      <li v-for="(fruta, index) in frutas" :key="index">
+        {{ index }} - {{ fruta.nombre }} - {{ fruta.cantidad }}
+          <p v-show="fruta.cantidad >= 5">
+            Hay 5 o más
+          </p>
+
+          <p v-show="fruta.cantidad <= 5">
+            Hay 5 o menos
+          </p>
+      </li>
+    </ul>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    numero: Number
+  },
+  data() {
+    return {
+      mivariable: "fgfdsghfhgfhgfhj",
+      frutas: [
+        {
+          nombre: "Manzana",
+          cantidad: 5
+        },
+        {
+          nombre: "Peras",
+          cantidad: 2
+        },
+        {
+          nombre: "Duraznos",
+          cantidad: 6
+        },
+        {
+          nombre: "Banano",
+          cantidad: 5
+        }
+    ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
