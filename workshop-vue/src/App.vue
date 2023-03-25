68f2876d9f6c0612a5c3462505f1937a5d23d53b
dgguzmangr <dgguzmangr@gmail.com>
2023-03-25 02:58:48 -0500

    JobOffer and App.vue html update

--- workshop-vue/src/App.vue
+++ workshop-vue/src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="app">
-    <nav class="container">
+    <nav class="container-fluid">
                 <!--section typeA (four columns, each colum img[align items center] + <p> under the img[align items left] )-->
-                <section class="row d-flex justify-content-center align-items-center text-center py-3" style="background-color: var(--white);">
-                    <div class="col text-left">
+                <section class="row d-flex justify-content-center align-items-center py-3" style="background-color: var(--white);">
+                    <div class="col-1 text-left">
                         <a href="index.html"><img loading="lazy" src="./assets/logo-blue.png" alt="PeakU"></a>
                     </div>
-                    <div class="col text-center">
-                        <div class="d-flex justify-content-between row list-unstyled font-weight-bold"
-                            style="font-family: var(--primaryFont); color: var(--colorB-1);">
-                            <li><a href="#" class="text-decoration-none text-reset">Empleos</a></li> <!--vínculo-->
-                            <li><a href="#" class="text-decoration-none text-reset">Bootcamp</a></li> <!--vínculo-->
-                            <li><a href="#" class="text-decoration-none text-reset">Acerca de nosotros</a></li> <!--vínculo-->
-                            <li><a href="#" class="text-decoration-none text-reset">Para profesionales</a></li> <!--vínculo-->
-                            <li><a href="#" class="text-decoration-none text-reset">Para empresas</a></li> <!--vínculo-->
-                        </div>
+                    <div class="col-8 text-center d-flex justify-content-between list-unstyled font-weight-bold flex-row" style="font-family: var(--primaryFont); color: var(--colorB-1);">
+                        <li><a href="#" class="text-decoration-none text-reset">Empleos</a></li> <!--vínculo-->
+                        <li><a href="#" class="text-decoration-none text-reset">Bootcamp</a></li> <!--vínculo-->
+                        <li><a href="#" class="text-decoration-none text-reset">Acerca de nosotros</a></li> <!--vínculo-->
+                        <li><a href="#" class="text-decoration-none text-reset">Para profesionales</a></li> <!--vínculo-->
+                        <li><a href="#" class="text-decoration-none text-reset">Para empresas</a></li> <!--vínculo-->
                     </div>
-                    <div class="col text-right">
+                    <div class="col-3 text-right">
                         <button class="btn1 float-right">Español</button>
                         <button class="btn1 float-right">Notificaciones</button>
                         <button class="btn2 float-right">Login</button>
@@ -43,8 +40,7 @@ export default {
   font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: justify;
   color: #212529;
-  margin-top: 60px;
 }
 </style>
